a9dcd6664720dbe70b06a25127a5dbf7950bf17c
Kelsha_Darby <ohanaipo@gmail.com>
2020-05-11 13:50:34 -0600

    Move github keys so test keys are below production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
   provider :github, ENV['PROD_GITHUB_CLIENT_ID'], ENV['PROD_GITHUB_CLIENT_SECRET']
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 end
