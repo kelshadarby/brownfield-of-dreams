123153c529598db62fbfc022f020ec5a498ae8b9
Jenny Klich <jennyklich@gmail.com>
2020-05-12 16:55:29 -0600

    Add progress to friendships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_173647) do
+ActiveRecord::Schema.define(version: 2020_05_12_213317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,11 @@ ActiveRecord::Schema.define(version: 2020_05_05_173647) do
     t.boolean "classroom", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_connections", id: false, force: :cascade do |t|
+    t.integer "user_a_id", null: false
+    t.integer "user_b_id", null: false
   end
 
   create_table "user_videos", force: :cascade do |t|
---
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "As a User" do
+  before :each do
+    user = User.create!(email: "jennyklich@gmail.com",
+                        first_name: "Jenny",
+                        last_name: "Klich",
+                        password: "password",
+                        role: 0,
+                        github_token: ENV['jenny_github_token'])
+    user_2 = User.create!(email: "kelsha@gmail.com",
+                        first_name: "Kelsha",
+                        last_name: "Darby",
+                        password: "password",
+                        role: 0,
+                        github_token: ENV['kelsha_github_token'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+  it "I can add followers/followings as friends if they have accounts", :vcr do
+    visit "/dashboard"
+
+    within '#follower-caachz' do
+      expect(page).to_not have_link("Add as Friend")
+    end
+  end
+end
